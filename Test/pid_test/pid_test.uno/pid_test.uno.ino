a92c344964d6d66e70e0c6874705fd9df005d67d
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-06-30 08:02:07 +0430

    refactor

--- Test/pid_test/pid_test.uno/pid_test.uno.ino
+++ Test/pid_test/pid_test.uno/pid_test.uno.ino
@@ -23,8 +23,8 @@ const float convert2 = 0.3; // converts raw y values to mm. found through manual
 #define MAP_Y(Y) ((274 - ((Y - 142) * 9.0 / 25.0)) * convert2)
 //Y: max=274, mid=137
 
-const int flatX = 79;
-const int flatY = 67;
+const int FLAT1 = 79;
+const int FLAT2 = 67;
 
 const int MAX_X = 571 * convert1;
 const int MAX_Y = 2110 * convert2;
@@ -39,8 +39,8 @@ const float OFFSET_Y = 25;
 const float OFFSET_X = 60;
 
 // PID values
-double setpoint1, input1, Output1; //for X
-double setpoint2, input2, Output2; //for Y
+double setpoint1, input1, output1; // for X
+double setpoint2, input2, output2; // for Y
 const int Ts = 100;
 
 // PID coefficients
@@ -57,44 +57,8 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 900);
 Servo servo1; // X axis
 Servo servo2; // Y axis
 
-PID myPID1(&input1, &Output1, &setpoint1, Kp1, Ki1, Kd1, DIRECT); // Controls X axis
-PID myPID2(&input2, &Output2, &setpoint2, Kp2, Ki2, Kd2, DIRECT); // Controls Y axis
-
-
-void setup()
-{
-  // PID Settings
-  myPID1.SetMode(AUTOMATIC);
-  myPID1.SetOutputLimits(19, 159);
-  myPID1.SetSampleTime(Ts);
-
-  myPID2.SetMode(AUTOMATIC);
-  myPID2.SetOutputLimits(17, 117);
-  myPID2.SetSampleTime(Ts);
-
-  // Attaching Servos
-  servo1.attach(SERVO1_PIN);
-  servo2.attach(SERVO2_PIN);
-  
-  // Using LED to show when stabilized
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-
-  // Using Serial to change setpoint and motion of ball
-  Serial.begin(9600);
-  
-  // Initialize Setpoints to center of plate
-  setpoint1 = MID_X;
-  setpoint2 = MID_Y;
-  
-  // Make plate flat
-  Output1 = flatX;
-  Output2 = flatY;
-  servo1.write(Output1);
-  servo2.write(Output2);
-  
-  delay(100);
-}
+PID PID1(&input1, &output1, &setpoint1, Kp1, Ki1, Kd1, DIRECT); // Controls X axis
+PID PID2(&input2, &output2, &setpoint2, Kp2, Ki2, Kd2, DIRECT); // Controls Y axis
 
 void switch_sp(void) {
   static double k = 0;
@@ -170,8 +134,8 @@ void switch_sp(void) {
         break;
     }
 
-    // myPID1.SetTunings(Kp1, Ki1, Kd1);
-    // myPID2.SetTunings(Kp2, Ki2, Kd2);
+    // PID1.SetTunings(Kp1, Ki1, Kd1);
+    // PID2.SetTunings(Kp2, Ki2, Kd2);
     
     // Serial.print("Delta="); Serial.print(delta);
     // Serial.print(", Kp1="); Serial.print(Kp1);
@@ -187,7 +151,7 @@ void switch_sp(void) {
       setpoint2 = MID_Y + 25 * sin(k);
       k = k - 0.0075;
       break;
-    case '*': // infinity motion
+    case '*': // infinity (lemniscate) motion
       setpoint1 = MID_X + (80 * cos(k)) / (1 + sin(k) * sin(k));
       setpoint2 = MID_Y + (70 * sin(k) * cos(k)) / (1 + sin(k) * sin(k));
       k = k + 0.005;
@@ -197,12 +161,48 @@ void switch_sp(void) {
   }
 }
 
+void setup()
+{
+  // PID Settings
+  PID1.SetMode(AUTOMATIC);
+  PID1.SetOutputLimits(FLAT1 - 60, FLAT1 + 60);
+  PID1.SetSampleTime(Ts);
+
+  PID2.SetMode(AUTOMATIC);
+  PID2.SetOutputLimits(FLAT2 - 50, FLAT2 + 50);
+  PID2.SetSampleTime(Ts);
+
+  // Attaching Servos
+  servo1.attach(SERVO1_PIN);
+  servo2.attach(SERVO2_PIN);
+  
+  // Using LED to show when stabilized
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // Using Serial to change setpoint and motion of ball
+  Serial.begin(9600);
+  
+  // Initialize Setpoints to center of plate
+  setpoint1 = MID_X;
+  setpoint2 = MID_Y;
+  
+  // Make plate flat
+  output1 = FLAT1;
+  output2 = FLAT2;
+  servo1.write(output1);
+  servo2.write(output2);
+  
+  delay(100);
+}
+
+
 void loop()
 {
   static unsigned long stable = 0;
   static unsigned int no_touch_count = 0;
   
-  while(stable < 50) // Regulation Loop
+  while (stable < 50) // Regulation Loop
   {
     switch_sp();
     
@@ -210,10 +210,11 @@ void loop()
     
     if (p.z > ts.pressureThreshhold) // Ball is on plate
     {
+      no_touch_count = 0;
+
       servo1.attach(SERVO1_PIN);
       servo2.attach(SERVO2_PIN);
       
-      no_touch_count = 0;
       TSPoint p = ts.getPoint();
       input1 = MAP_X(p.x);
       input2 = MAP_Y(p.y);
@@ -230,17 +231,16 @@ void loop()
         digitalWrite(LED_BUILTIN, LOW);
       }
       
-      myPID1.Compute(); // Computer action control on X axis
-      myPID2.Compute(); // Computer action control on Y axis
+      PID1.Compute(); // Computer action control on X axis
+      PID2.Compute(); // Computer action control on Y axis
     }
     else // There is no ball on plate
     {
       no_touch_count++;
-
       if (no_touch_count >= 300) 
       {
-        Output1 = flatX;
-        Output2 = flatY;
+        output1 = FLAT1;
+        output2 = FLAT2;
       }
       if (no_touch_count >= 800)
       {
@@ -249,9 +249,8 @@ void loop()
       }
     }
     
-    servo1.write(Output1);
-    servo2.write(Output2);
-          
+    servo1.write(output1);
+    servo2.write(output2);
   }
 
   servo1.detach();
